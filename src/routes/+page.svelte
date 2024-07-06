6bbc1bb87e1582bc956212ebe7add8247972c06e
Ackmanx <7490057+ackmanx@users.noreply.github.com>
2024-07-06 13:33:28 -0500

    Add stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,22 +2,31 @@
   import { page } from '$app/stores'
   import { DateTime } from 'luxon'
 
-  const stuff = $page.data.collectionDocuments
+  const documents = $page.data.collectionDocuments
+
+  const stylesForDocs = documents.map(() => {
+    const top = Math.floor(Math.random() * 100)
+    const left = Math.floor(Math.random() * 100)
+
+    return `position: absolute; top: ${top}vh; left: ${left}vw`
+  })
 </script>
+
+<style>
+  div {
+    overflow: hidden;
+  }
+</style>
 
 <svelte:head>
   <title>I Already Saw That</title>
 </svelte:head>
 
-<!-- todo: left off here... create a random placement of each date with the most recent being bigger and a different color -->
-
 <!-- svelte-ignore component_name_lowercase -->
 <page>
-  <ul>
-    {#each stuff as doc}
-      <li>
-        {DateTime.fromISO(doc.createdForTesting).toRelative()}
-      </li>
-    {/each}
-  </ul>
+  {#each documents as doc, index}
+    <div style={stylesForDocs[index]}>
+      {DateTime.fromISO(doc.createdForTesting).toRelative()}
+    </div>
+  {/each}
 </page>
